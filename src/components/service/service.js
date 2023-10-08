07fb4ab8b239fc5c3d3fdc1176f08f61d6b6b461
Margarita Bershtainis <margo.bershtainis@gmail.com>
2023-10-08 12:33:32 +0300

    try commit

--- src/components/service/service.js
+++ src/components/service/service.js
@@ -48,7 +48,7 @@ export default class Service {
          },
          ...bod
        }).catch((res) => {
-         console.log('FETCH ERROR ',res)
+         console.log('FETCH ERROR ', res)
          return res;
       })
       return result;
